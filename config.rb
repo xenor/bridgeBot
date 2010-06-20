929287948c32d2df9338a4444286f8f8170e5a28
xenor <xenor@lifefight.de>
2010-06-20 18:08:23 +0200

    dev

---
+++ config.rb
@@ -0,0 +1,10 @@
+# config.rb
+
+# uplink configuration
+$server_host = "farmer.freenode.net"
+$server_port = 6667
+
+# bot configuration
+$bot_ident = "bridge"
+$bot_realname = "Bridge - visist #crapcode"
+$bot_nick = "BridgeBot"
